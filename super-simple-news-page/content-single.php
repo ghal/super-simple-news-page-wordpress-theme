52637d18962c8b1e3e16be1c0b3e116c2508d0ef
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-09-27 16:41:07 +0300

    created single page

--- super-simple-news-page/content-single.php
+++ super-simple-news-page/content-single.php
@@ -2,28 +2,33 @@
 /**
  * @package super simple news page
  */
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php super_simple_news_page_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
+?>			<style type="text/css">
+				.article #article-img{
+				width: 100%;
+				background-image: url("<?php
+					$image_id = get_post_thumbnail_id();
+					$image_url = wp_get_attachment_image_src($image_id,'large', true);
+					echo $image_url[0];
+				 ?>");
+				background-position: top center;
+				background-repeat: no-repeat;
+				background-size: cover;
+				height: 400px;
+				}
+			</style>
+            <div class="article">
+            	<div class="entry-meta">
+					<?php super_simple_news_page_posted_on(); ?>
+				</div><!-- .entry-meta -->
+                <div id="article-img"></div>
+                <?php the_title( '<h1>', '</h1>' ); ?>
+                <div id="article-content">
+					<?php the_content(); ?>
+				</div>
+            </div>
+            		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'super-simple-news-page' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php super_simple_news_page_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
